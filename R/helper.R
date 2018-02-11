e6da4902f08d3c7315a0d2dda50fa4ea0375778b
Vince Polito <vince4223@gmail.com>
2018-02-11 12:44:48 +1100

    Initial commit

--- R/helper.R
+++ R/helper.R
@@ -1,39 +1,86 @@
 # VP Helper Functions
 # 10/2/18
-
-# if (!require("pacman")) install.packages("pacman")
-# library(pacman)
-# p_load(tidyverse,psych)  
-
 #############
 # HELPER FUNCTIONS
 
+#' A better t-test
+#'
+#' @param x First vector to compare.
+#' @param y Second vector to compare.
+#' @param paired Set FALSE (default) for independent test, TRUE for paired test
+#' @return Neat t-test output
+#' @examples
+#' v.test(somevar, someothervar)
+#' @export
 v.test<-function(x,y,paired=FALSE){
   t<-t.test(x,y,paired=paired)
   return(paste0("t = ",r(t$statistic),", p = ",pv(t$p.value),", 95% CI = [",r(t$conf.int[1]),", ",r(t$conf.int[2]),"]"))
 }
 
+#' Quick rounding
+#'
+#' @param x vector to be rounded.
+#' @param dp Number of decimal places (defaults to 2).
+#' @return Rounded output
+#' @examples
+#' r(4.66544645454)
+#' r(somevector, 3)
+#' @export
 r<-function(x, dp=2){ #easy rounding
   as.numeric(format(round(x,dp),nsmall=dp))
 }
 
+#' round_df function from http://jeromyanglim.tumblr.com/post/50228877196/round-numbers-in-data-frame-that-contains-non
+#'
+#' @param x a dataframe.
+#' @param digits Number of digits to round to.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' tocome
+#' @export
 round_df <- function(x, digits) {
-  # round all numeric variables
-  # x: data frame 
-  # digits: number of digits to round
   numeric_columns <- sapply(x, class) == 'numeric'
   x[numeric_columns] <-  round(x[numeric_columns], digits)
-  x
+  print(x)
 }
 
+#' pv neatly formats the significance of a p-value
+#'
+#' @param x A p-value
+#' @return Neatly formatted p-output
+#' @examples
+#' pv(dataobject$p.value)
+#' @export
 pv<-function(x){
   if (x < 0.0005) { # adapted from http://my.ilstu.edu/~wjschne/444/IndependentSamples.html#(18). Annoying to have to do this!
-    x <- "< 0.001" 
-  } else { 
-    x <- as.character(paste0(x %>% formatC(digits = 3, format = "f"))) 
+    x <- "< 0.001"
+  } else {
+    x <- as.character(paste0(x %>% formatC(digits = 3, format = "f")))
   }
   return(x)
 }
 
-#############
+#' SimpleCap capitalises all first letters in a string
+#'
+#' @param x A string
+#' @return Capitalised String
+#' @examples
+#' tocome
+#' @export
+simpleCap <- function(x) { #capitalise all first letters
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
 
+#' vmode shows the most common value in a vector
+#'
+#' @param x A vector
+#' @return mode value within the vector
+#' @examples
+#' tocome
+#' @export
+vmode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+vtools
+======
+
+a simple collection of R functions
+
+Example
+-------
+
+Helps with rounding. Better t-test
+
+``` r
+## basic example code
+```
